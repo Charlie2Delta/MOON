0c1426dbeaa71ec8d7b5f059db215320bcf0d12d
YungBandulf <p.a.andrighetti@gmail.com>
2024-10-03 16:27:52 +0200

    Tweaks to code

--- Contrats/Moon.sol
+++ Contrats/Moon.sol
@@ -3,34 +3,33 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+// TTP = Trusted Third Party
 
 contract Moon is ERC721, Ownable {
-    // Whitelist mapping and array for tracking Trusted Third Party (TTP) addresses
     mapping(address => bool) private whitelist;
     address[] private whitelistedAddresses;
     
-    // Structure representing properties of an NFT
     struct NFTProperties {
-        address currentPosition;
+        address position;
         address holder;
         address proposedPosition;
         bool holderApprovedChange;
         bool proposedApprovedChange;
     }
     
-    // Mappings for NFT properties and tracking NFTs held by addresses
     mapping(uint256 => NFTProperties) private nftProperties;
-    mapping(address => uint256[]) private holderToNFTs;
+    mapping(address => uint256[]) private holderToNFTs; // Track NFTs held by each address
     uint256 private _currentTokenId = 0;
 
-    // Constructor: initializes contract with a whitelist and sets the owner as a TTP
-    constructor(address[] memory initialWhitelist) ERC721("Moon", "MOON") {
-        // Add the contract owner to the whitelist
-        address contractOwner = msg.sender;
-        whitelist[contractOwner] = true;
-        whitelistedAddresses.push(contractOwner);
+    constructor(address[] memory initialWhitelist) ERC721("Moon", "MOON") Ownable(msg.sender) {
+        //⚠ CAREFUL WITH THE STATEMENT BELOW ⚠
+        // Add the contract deployer (owner) to the whitelist
+        address owner = msg.sender;
+        whitelist[owner] = true;
+        whitelistedAddresses.push(owner);
 
-        // Add provided addresses to the whitelist
+        //⚠ CAREFUL WITH THE STATEMENT BELOW ⚠
+        // Add the addresses from the initial whitelist
         for (uint256 i = 0; i < initialWhitelist.length; i++) {
             address addr = initialWhitelist[i];
             if (!whitelist[addr]) {
@@ -40,82 +39,88 @@ contract Moon is ERC721, Ownable {
         }
     }
 
-    // Modifier to restrict function access to the NFT holder
+    // Modifier to restrict functions to the holder of the NFT, maybe obsolete after changes 
     modifier onlyHolder(uint256 tokenId) {
-        require(msg.sender == nftProperties[tokenId].holder, "Caller is not the holder of the NFT");
+        require(msg.sender == nftProperties[tokenId].holder, "Caller is not the holder");
         _;
     }
 
     // Adds an address to the TTP whitelist
     function addToWhitelist(address _address) external onlyOwner {
-        require(!whitelist[_address], "Address is already whitelisted");
+        require(!whitelist[_address], "Address is already whitelisted as a TTP");
         whitelist[_address] = true;
         whitelistedAddresses.push(_address);
     }
 
-    // Returns true if an address is whitelisted as a TTP
+    // View function that checks if an address is in the TTP whitelist
     function isWhitelisted(address _address) external view returns (bool) {
         return whitelist[_address];
     }
 
-    // Retrieves all addresses in the TTP whitelist
+    // View function to get all the addresses that are in the TTP whitelist
     function getWhitelistedAddresses() external view returns (address[] memory) {
         return whitelistedAddresses;
     }
 
-    // Initiates a position change to a new TTP address
-    function initiateSend(uint256 tokenId, address _newPosition) external onlyHolder(tokenId) {
+    // Function to initiate the change of position from the holder to the desired TTP address -- SHIPMENT?
+    function initiateSend(uint256 tokenId, address _newPosition) external {
+        uint256 selectedTokenId = _getSelectedTokenId(tokenId);
+
         require(whitelist[_newPosition], "New position must be a whitelisted TTP address");
-        nftProperties[tokenId].proposedPosition = _newPosition;
-        nftProperties[tokenId].holderApprovedChange = true;
-        nftProperties[tokenId].proposedApprovedChange = false;
+        nftProperties[selectedTokenId].proposedPosition = _newPosition;
+        nftProperties[selectedTokenId].holderApprovedChange = true;
+        nftProperties[selectedTokenId].proposedApprovedChange = false;
     }
 
-    // Approves a proposed position change; only callable by the proposed TTP address
+    // Function to approve said change of position, should be linked to the app and thus the chip
     function approveSend(uint256 tokenId) external {
-        require(msg.sender == nftProperties[tokenId].proposedPosition, "Caller is not the proposed TTP address");
-        require(nftProperties[tokenId].holderApprovedChange, "Position change not initiated by the holder");
+        require(msg.sender == nftProperties[tokenId].proposedPosition, "Only the proposed TTP address can approve the change");
+        require(nftProperties[tokenId].holderApprovedChange, "Holder has not initiated a position change");
 
         nftProperties[tokenId].proposedApprovedChange = true;
 
         if (nftProperties[tokenId].holderApprovedChange && nftProperties[tokenId].proposedApprovedChange) {
-            nftProperties[tokenId].currentPosition = nftProperties[tokenId].proposedPosition;
+            nftProperties[tokenId].position = nftProperties[tokenId].proposedPosition;
             nftProperties[tokenId].holderApprovedChange = false;
             nftProperties[tokenId].proposedApprovedChange = false;
             nftProperties[tokenId].proposedPosition = address(0);
         }
     }
 
-    // Initiates the return of the NFT's position to the holder
-    function initiateGrab(uint256 tokenId) external onlyHolder(tokenId) {
-        require(nftProperties[tokenId].currentPosition != nftProperties[tokenId].holder, "NFT is already with the holder");
-        nftProperties[tokenId].proposedPosition = nftProperties[tokenId].holder;
-        nftProperties[tokenId].holderApprovedChange = true;
-        nftProperties[tokenId].proposedApprovedChange = false;
+    // Function to initiate the change of position from the TTP address to the holder
+    function initiateGrab(uint256 tokenId) external {
+        uint256 selectedTokenId = _getSelectedTokenId(tokenId);
+
+        require(nftProperties[selectedTokenId].position != nftProperties[selectedTokenId].holder, "Holder already has position");
+        nftProperties[selectedTokenId].proposedPosition = nftProperties[selectedTokenId].holder;
+        nftProperties[selectedTokenId].holderApprovedChange = true;
+        nftProperties[selectedTokenId].proposedApprovedChange = false;
     }
 
-    // Approves the return of the NFT's position to the holder; callable by the current position's TTP
+    // Function to approve said change of position, should be linked to the app and thus the chip -- SHIPMENT?
     function approveGrab(uint256 tokenId) external {
-        require(msg.sender == nftProperties[tokenId].currentPosition, "Caller is not the current position owner");
-        require(nftProperties[tokenId].holderApprovedChange, "Position change not initiated");
+        require(msg.sender == nftProperties[tokenId].position, "Only the current position owner can approve the change");
+        require(nftProperties[tokenId].holderApprovedChange, "Holder has not initiated a change");
 
         nftProperties[tokenId].proposedApprovedChange = true;
 
         if (nftProperties[tokenId].holderApprovedChange && nftProperties[tokenId].proposedApprovedChange) {
-            nftProperties[tokenId].currentPosition = nftProperties[tokenId].holder;
+            nftProperties[tokenId].position = nftProperties[tokenId].holder;
             nftProperties[tokenId].holderApprovedChange = false;
             nftProperties[tokenId].proposedApprovedChange = false;
             nftProperties[tokenId].proposedPosition = address(0);
         }
     }
 
-    // Mints a new NFT and assigns its initial properties
+    // Mints an NFT
     function mintNFT(address _to) external onlyOwner {
         uint256 newTokenId = _currentTokenId + 1;
         _currentTokenId = newTokenId;
 
+        //⚠ CAREFUL WITH THE STATEMENT BELOW ⚠
+        // Initialize NFT properties with the owner as the initial position
         nftProperties[newTokenId] = NFTProperties({
-            currentPosition: owner(),
+            position: owner(),
             holder: _to,
             proposedPosition: address(0),
             holderApprovedChange: false,
@@ -123,39 +128,40 @@ contract Moon is ERC721, Ownable {
         });
 
         _safeMint(_to, newTokenId);
+        
         holderToNFTs[_to].push(newTokenId);
     }
 
-    // Retrieves the current position of a specified NFT
+    // View function to track a specific NFT's position
     function getPosition(uint256 tokenId) external view returns (address) {
-        return nftProperties[tokenId].currentPosition;
+        return nftProperties[tokenId].position;
     }
 
-    // Retrieves the most recent token ID minted
+    // View function to track the last token minted
     function currentTokenId() external view returns (uint256) {
         return _currentTokenId;
     }
 
-    // Returns all NFTs held by a specific holder
+    // Function to get all NFTs held by a holder
     function getNFTsByHolder(address holder) external view returns (uint256[] memory) {
         return holderToNFTs[holder];
     }
 
-    // Private function to get the selected tokenId, choosing automatically if only one is held
+    // Private function to select the tokenId automatically when possible
     function _getSelectedTokenId(uint256 tokenId) private view returns (uint256) {
         uint256[] memory heldTokens = holderToNFTs[msg.sender];
         require(heldTokens.length > 0, "Caller does not hold any NFT");
 
         if (heldTokens.length == 1) {
-            return heldTokens[0];
+            return heldTokens[0]; // Automatically select the only NFT
         } else {
-            require(tokenId > 0, "Token ID required when holding multiple NFTs");
+            require(tokenId > 0, "Token ID must be provided when holding multiple NFTs");
             require(_isHeldByCaller(tokenId), "Caller does not hold the specified NFT");
             return tokenId;
         }
     }
 
-    // Helper function to check if the caller holds a specific tokenId
+    // Helper function to check if the token ID is held by the caller
     function _isHeldByCaller(uint256 tokenId) private view returns (bool) {
         uint256[] memory heldTokens = holderToNFTs[msg.sender];
         for (uint256 i = 0; i < heldTokens.length; i++) {
@@ -166,14 +172,13 @@ contract Moon is ERC721, Ownable {
         return false;
     }
 
-    // Internal function to update NFT holder information upon transfer, ensuring whitelist validation
+    // Override _update function to block transfers if position is not a whitelisted TTP, insuring that tracability is maintained. Also updates holders informations
     function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
-        require(whitelist[nftProperties[tokenId].currentPosition], "Transfer denied: Current position is not whitelisted");
 
-        // Call the parent contract's _update function
+        require(whitelist[nftProperties[tokenId].position], "Transfer not allowed: Position is not a whitelisted TTP");
+
         address previousOwner = super._update(to, tokenId, auth);
 
-        // Remove token from the previous holder's list
         if (previousOwner != address(0)) {
             uint256[] storage fromTokens = holderToNFTs[previousOwner];
             for (uint256 i = 0; i < fromTokens.length; i++) {
@@ -185,12 +190,10 @@ contract Moon is ERC721, Ownable {
             }
         }
 
-        // Add token to the new holder's list
         if (to != address(0)) {
             holderToNFTs[to].push(tokenId);
         }
 
-        // Update holder information
         if (to != address(0)) {
             nftProperties[tokenId].holder = to;
         }
